8f0a6d5ecb42b0f2f9b51d7d5536aff8a6d49204
89mpxf <diamond061@outlook.com>
2023-03-29 09:23:35 -0400

    Fixed metadata system & added shared commands

--- server/src/protocol/commands/all/HELP.py
+++ server/src/protocol/commands/all/HELP.py
@@ -8,7 +8,7 @@ __help_text = "Displays help"
 def main(interpreter, command: str = None):
     if command is not None:
         try:
-            interpreter.client.send(interpreter.cipher.encrypt(f"200 Help for {command}:\r\nUsage: {command} {interpreter.commands[interpreter.interpreter_mode]['meta'][command]['usage']}\r\n{interpreter.commands[interpreter.interpreter_mode]['meta'][command]['help_text']}".encode()))
+            interpreter.client.send(interpreter.cipher.encrypt(f"200 Help for {command}:\r\nUsage: {command} {interpreter.commands['meta'][interpreter.interpreter_mode][command]['usage']}\r\n{interpreter.commands['meta'][interpreter.interpreter_mode][command]['help_text']}".encode()))
             if interpreter.debug:
                 log(f"session-{interpreter.id}/interpreter", f"Command HELP returned 200.")
         except:
@@ -16,6 +16,6 @@ def main(interpreter, command: str = None):
             if interpreter.debug:
                 log(f"session-{interpreter.id}/interpreter", f"Command HELP returned 420.")
     else:
-        interpreter.client.send(interpreter.cipher.encrypt("200 Available commands:\r\n".encode() + "\r\n".join([str(command + ' - ' + interpreter.commands[interpreter.interpreter_mode]["meta"][command]["help_text"]) for command in interpreter.commands[interpreter.interpreter_mode]['meta']]).encode() + "\r\nRun HELP <command> for more information.\r\n".encode()))
+        interpreter.client.send(interpreter.cipher.encrypt("200 Available commands:\r\n".encode() + "\r\n".join([str(command + ' - ' + interpreter.commands["meta"][interpreter.interpreter_mode][command]["help_text"]) for command in interpreter.commands['meta'][interpreter.interpreter_mode]]).encode() + "\r\nRun HELP <command> for more information.\r\n".encode()))
         if interpreter.debug:
             log(f"session-{interpreter.id}/interpreter", f"Command HELP returned 200.")
--- server/src/protocol/load.py
+++ server/src/protocol/load.py
@@ -6,21 +6,30 @@ from os import listdir
 @staticmethod
 def load_commands() -> dict:
     commands = {}
+    commands["meta"] = {}
     for dir in listdir(dirname(abspath(__file__)) + "/commands"):
         if dir != "__pycache__" and isdir(dirname(abspath(__file__)) + "/commands/" + dir):
             commands[dir] = {}
-            commands[dir]["meta"] = {}
+            commands["meta"][dir] = {}
             for file in listdir(dirname(abspath(__file__)) + "/commands/" + dir):
                 if file != "__pycache__" and file.endswith(".py"):
                     command = __import__(f"src.protocol.commands.{dir}.{file[:-3]}", fromlist=[file[:-3]])
                     commands[dir][file[:-3]] = getattr(command, "main")
-                    commands[dir]["meta"][file[:-3]] = {}
+                    commands["meta"][dir][file[:-3]] = {}
                     try:
-                        commands[dir]["meta"][file[:-3]]["usage"] = getattr(command, "__usage")
+                        commands["meta"][dir][file[:-3]]["usage"] = getattr(command, "__usage")
                     except:
-                        commands[dir]["meta"][file[:-3]]["usage"] = ""
+                        commands["meta"][dir][file[:-3]]["usage"] = ""
                     try:
-                        commands[dir]["meta"][file[:-3]]["help_text"] = getattr(command, "__help_text")
+                        commands["meta"][dir][file[:-3]]["help_text"] = getattr(command, "__help_text")
                     except:
-                        commands[dir]["meta"][file[:-3]]["help_text"] = "(No help text provided)"
+                        commands["meta"][dir][file[:-3]]["help_text"] = "(No help text provided)"
+    if "all" in commands:
+        for mode in [mode for mode in commands if mode != "meta" and mode != "all"]:
+            for command in commands["all"]:
+                if command not in commands[mode]:
+                    commands[mode][command] = commands["all"][command]
+                    commands["meta"][mode][command] = commands["meta"]["all"][command]
+    del commands["all"]
+    del commands["meta"]["all"]
     return commands

1898b4ddb6205e3c05ee1e2d69c30dfad1fc2fbc
89mpxf <diamond061@outlook.com>
2023-04-20 11:01:37 -0400

    Updated version switch output

--- server/src/subprogram/version.py
+++ server/src/subprogram/version.py
@@ -1,3 +1,8 @@
+from platform import system, release, python_version_tuple, python_implementation, python_compiler
+
 def version(compat_signature: tuple[str, int, int, int]):
-    print(f"{compat_signature[0]} (v{compat_signature[1]}.{compat_signature[2]}.{compat_signature[3]})")
+    _pyver = python_version_tuple()
+    print(f"Server Version: {compat_signature[0]} (v{compat_signature[1]}.{compat_signature[2]}.{compat_signature[3]})")
+    print(f"Operating System: {system()} {release()}")
+    print(f"Python Version: {python_implementation()} {_pyver[0]}.{_pyver[1]}.{_pyver[2]} ({python_compiler()})")
     return 0
